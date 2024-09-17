9907c7834a67594c14a4f3fd077f5f6720e353d3
DevNicolas <devnicolasbraga@gmail.com>
2024-09-17 15:20:24 -0300

    Dia 11: Finalizando a sessão sobre Mongoose, fiz a mudança do codigo de MongoDB para a sua ODM Mongoose, facilitando muito o CRUD.

---
+++ 13_MONGOOSE/5_lendo_dado_individual/controllers/ProductController.js
@@ -0,0 +1,73 @@
+const Product = require('../models/Product')
+
+module.exports = class ProductController {
+    static async showProducts(req, res) {
+        const products = await Product.find().lean()
+
+        res.render('products/all', { products })
+    }
+
+    static createProducts(req, res) {
+        res.render('products/create')
+    }
+
+    static async createProductsPost(req, res) {
+
+        const name = req.body.name
+        const image = req.body.image
+        const price = req.body.price
+        const description = req.body.description
+
+        const product = new Product({name, image, price, description})
+
+        await product.save()
+
+        res.redirect('/products')
+    }
+
+     static async getProduct(req, res) {
+
+        const id = req.params.id
+
+        const product = await Product.findById(id).lean()
+
+        res.render('products/product', { product })
+
+     }
+
+    // tatic async removeProduct(req, res) {
+
+    //    const id = req.params.id
+
+    //    await Product.removeProductById(id)
+
+    //    res.redirect('/products')
+    // 
+
+    // tatic async editProduct(req, res) {
+
+    //    const id = req.params.id
+
+    //    const product = await Product.getProductById(id)
+
+    //    res.render('products/edit', { product })
+
+    // 
+
+    // tatic async editProductPost(req, res) {
+
+    //    const id = req.body.id
+    //    const name = req.body.name
+    //    const image = req.body.image
+    //    const price = req.body.price
+    //    const description = req.body.description
+
+    //    const product = new Product(name, image, price, description)
+
+    //    await product.updateProduct(id)
+
+    //    res.redirect('/products')
+
+    // 
+
+}
---
+++ 13_MONGOOSE/7_editando_dado_post/routes/productsRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+const ProductController = require('../controllers/ProductController')
+
+router.get('/create', ProductController.createProducts)
+router.post('/create', ProductController.createProductsPost)
+// router.post('/remove/:id', ProductController.removeProduct)
+router.post('/edit', ProductController.editProductPost)
+router.get('/edit/:id', ProductController.editProduct)
+router.get('/:id', ProductController.getProduct)
+router.get('/', ProductController.showProducts)
+
+
+
+module.exports = router
